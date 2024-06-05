c1e1339c6b26df193c4535304f9d1b2e55601876
abp1234 <156388823+abp1234@users.noreply.github.com>
2024-06-05 23:16:22 +0900

    [Platinum V] Title: 히스토그램에서 가장 큰 직사각형, Time: 72 ms, Memory: 7020 KB -BaekjoonHub

---
+++ 백준/Platinum/6549. 히스토그램에서 가장 큰 직사각형/히스토그램에서 가장 큰 직사각형.cc
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <tuple>
+using namespace std;
+
+int main(void) {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	int N;
+	long long temp;
+	cin >> N;
+	while (N != 0) {
+		priority_queue<tuple<long long, long long>> arr;
+		vector<long long>check(N + 4);
+		for (int i = 0; i < N; i++) {
+			cin >> temp;
+			arr.push(make_tuple(temp, i + 1));
+		}
+		long long heigh, index;
+		long long max_size = 0LL;
+		while (!arr.empty()) {
+			tie(heigh, index) = arr.top();
+			arr.pop();
+			check[index] = check[index - 1] + 1;
+			if (check[index - 1] != 0) {
+				check[index - check[index - 1]] = check[index];
+			}
+			if (check[index + 1] != 0) {
+				temp = check[index] + check[index + 1];
+				check[index + check[index + 1]] = temp;
+				check[index + 1 - check[index]] = temp;
+				max_size = max(temp * heigh, max_size);
+			}
+
+			max_size = max(check[index] * heigh, max_size);
+			if (heigh * N <= max_size) break;
+		}
+		cout << max_size << '\n';
+		cin >> N;
+	}
+	
+	return 0;
+}
