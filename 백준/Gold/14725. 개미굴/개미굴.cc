6390c3f80cd0f69294bc7e71a9f50e21df159b6d
abp1234 <156388823+abp1234@users.noreply.github.com>
2024-07-07 17:05:30 +0900

    [Gold III] Title: 개미굴, Time: 32 ms, Memory: 2168 KB -BaekjoonHub

--- 백준/Gold/14725. 개미굴/개미굴.cc
+++ 백준/Gold/14725. 개미굴/개미굴.cc
@@ -55,3 +55,127 @@ int main() {
 
 	return 0;
 }
+
+
+
+/*
+#include <iostream>
+#include <string>
+#include <map>
+using namespace std;
+
+struct TrieNode {
+	map<string, TrieNode*> children;
+	bool isEndOfWord;
+
+	TrieNode() : isEndOfWord(false) {}
+};
+
+class Trie {
+private:
+	TrieNode* root;
+
+public:
+	Trie() {
+		root = new TrieNode();
+	}
+
+	~Trie() {
+		deleteTrie(root);
+	}
+
+	void insert(vector<string>& words) {
+		TrieNode* current = root;
+		for (const string& word : words) {
+			if (current->children.find(word) == current->children.end()) {
+				current->children[word] = new TrieNode();
+			}
+			current = current->children[word];
+		}
+		current->isEndOfWord = true;
+	}
+
+	void deleteTrie(TrieNode* node) {
+		for (auto& child : node->children) {
+			deleteTrie(child.second);
+		}
+		delete node;
+	}
+};
+
+int main(void) {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int N, k;
+	string str;
+	cin >> N;
+
+	Trie trie;
+	for (int i = 0; i < N; i++) {
+		cin >> k;
+		vector<string> words;
+		for (int j = 0; j < k; j++) {
+			cin >> str;
+			words.push_back(str);
+		}
+		trie.insert(words);
+	}
+
+	return 0;
+}
+
+struct Trie {
+	bool finish;
+	Trie* next[26];
+
+	Trie() : finish(false) {
+		memset(next, 0, sizeof(next));
+	}
+
+	~Trie() {
+		for (int i = 0; i < 26; i++)
+			if (next[i])
+				delete next[i];
+	}
+
+	void insert(const char* key) {
+		if (*key == '\0')
+			finish = true;
+		else {
+			int curr = *key - 'A';
+			if (next[curr] == NULL)
+				next[curr] = new Trie();
+			next[curr]->insert(key + 1);
+		}
+	}
+
+	Trie* find(const char* key) {
+		if (*key == '\0')return this;
+		int curr = *key - 'A';
+		if (next[curr] == NULL)return NULL;
+		return next[curr]->find(key + 1);
+	}
+};
+
+int main(void) {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	int N, k;
+	string str;
+	cin >> N;
+
+	Trie* root = new Trie();
+	for (int i = 0; i < N; i++) {
+		cin >> k;
+		for (int j = 0; j < k; j++) {
+			cin >> str;
+			root->insert(str.c_str());
+		}
+	}
+
+	delete root;
+	return 0;
+}*/
