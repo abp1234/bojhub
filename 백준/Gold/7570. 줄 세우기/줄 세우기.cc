0e278adffd823c9e6dbbb0521414f316bfaa0d36
abp1234 <156388823+abp1234@users.noreply.github.com>
2024-11-03 22:25:12 +0900

    [Gold II] Title: 줄 세우기, Time: 396 ms, Memory: 5928 KB -BaekjoonHub

---
+++ 백준/Gold/7570. 줄 세우기/줄 세우기.cc
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+struct Info {
+	int idx;
+	Info(int i):idx(i){}
+};
+
+int fn(int N) {
+	vector<Info> p(N + 1, Info(0));
+	int lg = 0, ct = 1;
+
+	for (int i = 1; i <= N; ++i) {
+		int num;
+		cin >> num;
+		p[num].idx = i;
+	}
+
+	for (int i = 1; i < N; ++i) {
+		if (p[i].idx < p[i + 1].idx) {
+			ct++;
+		}
+		else {
+			lg = max(lg, ct);
+			ct = 1;
+		}
+	}
+
+	lg = max(lg, ct);
+
+	return N - lg;
+}
+
+int main() {
+	int N;
+	cin >> N;
+	cout << (N != 1 ? fn(N) : 0) << '\n';
+	return 0;
+}
