a162d21a71c349c002900df94b5db8928913cd99
abp1234 <156388823+abp1234@users.noreply.github.com>
2024-12-22 19:35:42 +0900

    [level 3] Title: 헤비 유저가 소유한 장소, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/3/77487. 헤비 유저가 소유한 장소/헤비 유저가 소유한 장소.sql
@@ -0,0 +1,20 @@
+-- 코드를 입력하세요
+SELECT
+    P.ID,
+    P.NAME,
+    P.HOST_ID
+FROM
+    PLACES P
+WHERE
+    P.HOST_ID IN(
+        SELECT
+            HOST_ID
+        FROM
+            PLACES
+        GROUP BY
+            HOST_ID
+        HAVING
+            COUNT(*)>=2
+    )
+ORDER BY
+    P.ID;
