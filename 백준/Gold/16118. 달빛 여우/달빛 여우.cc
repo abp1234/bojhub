c325cf996d17320b1773d65e3e448fdeb35f7440
abp1234 <156388823+abp1234@users.noreply.github.com>
2025-08-04 23:42:02 +0900

    [Gold I] Title: 달빛 여우, Time: 68 ms, Memory: 10764 KB -BaekjoonHub

--- 백준/Gold/16118. 달빛 여우/달빛 여우.cc
+++ 백준/Gold/16118. 달빛 여우/달빛 여우.cc
@@ -17,12 +17,12 @@ int main() {
     vector<vector<pair<int,int>>> tree(N);
     for (int i = 0; i < M; i++) {
         cin >> a >> b >> d;
-        a--; b--;  
+        a--; b--;  // 1-based → 0-based 변환
         tree[a].emplace_back(b, d * 2);
         tree[b].emplace_back(a, d * 2);
     }
 
-
+    // 늑대: 일반 다익스트라 (노드번호 오류 수정)
     vector<int> wolf(N, INF);
     priority_queue<pair<int,int>, vector<pair<int,int>>, greater<>> pq;
     wolf[0] = 0;
@@ -31,15 +31,17 @@ int main() {
     while(!pq.empty()) {
         auto [cost, cur] = pq.top();
         pq.pop();
+        if (wolf[cur] < cost) continue;
+
         for (auto [next, w] : tree[cur]) {
             if (wolf[next] > cost + w) {
                 wolf[next] = cost + w;
-                pq.emplace(wolf[next], next);  
+                pq.emplace(wolf[next], next);  // next 노드를 넣어야 함!
             }
         }
     }
 
-    
+    // 여우: 상태 0 = 빠르게, 1 = 느리게
     vector<vector<int>> fox(N, vector<int>(2, INF));
     priority_queue<tuple<int,int,int>, vector<tuple<int,int,int>>, greater<>> pq2;
     fox[0][0] = 0;
@@ -65,7 +67,7 @@ int main() {
     }
 
     int cnt = 0;
-    for (int i = 1; i < N; i++) {  
+    for (int i = 1; i < N; i++) {  // 시작점 0 제외
         if (wolf[i] < min(fox[i][0], fox[i][1])) cnt++;
 
     }
